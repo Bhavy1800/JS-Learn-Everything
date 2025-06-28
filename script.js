35c5b3fdfce44bf72882bcef29ac38f4ebe2bf49
Bhavy Patel <bhavyapatel1800@gmail.com>
2025-06-28 21:23:38 +0530

    started learning variables

---
+++ script.js
@@ -0,0 +1,10 @@
+// var let const - line by line comparison
+
+var a = 12; // accessible in whole program
+
+a = 12; // this is a bug/problem
+var a;
+var a = 12;
+let a;
+let a = 12;
+const a = 12;
